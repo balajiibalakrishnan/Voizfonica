9e89395d9028ff1175feeb641075305859cd0eef
HariSakthi <hariharanworld4@gmail.com>
2019-08-14 14:30:12 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Model/Order.java
@@ -0,0 +1,29 @@
+package com.example.demo.Model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+@Document
+@Data
+public class Order {
+    @Id
+    private String id;
+
+   private String user;
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    private Dongle dongle;
+   private Payment payment;
+   private PostPaid postPaid;
+   private PrepaidPlan prepaidplan;
+
+
+}
